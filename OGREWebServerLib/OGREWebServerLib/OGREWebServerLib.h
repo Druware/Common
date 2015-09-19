3eefead6561b27f2dc7604cdb99048e073967f47
Dru Satori <dru@satori-assoc.com>
2015-09-19 10:17:57 -0400

    Initial Commit

---
+++ OGREWebServerLib/OGREWebServerLib/OGREWebServerLib.h
@@ -0,0 +1,13 @@
+//
+//  OGREWebServerLib.h
+//  OGREWebServerLib
+//
+//  Created by Andy Satori on 9/19/15.
+//  Copyright © 2015 O.G.R.E. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OGREWebServerLib : NSObject
+
+@end
