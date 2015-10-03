ca7077d9652ad2def317101fb2f684d86fabb116
Dru Satori <dru@satori-assoc.com>
2015-10-03 18:59:14 -0400

    Started login support. Added test HTML page to the Repo. Will need to rework it to be pretty and make sense.

--- OGREDataServiceLib/pgsql-data-schema.sql
+++ OGREDataServiceLib/pgsql-data-schema.sql
@@ -57,12 +57,13 @@ create table web."user" (
     contact_info varchar(255) null
 );
 
-create table web."session" (
+create table web."sessio n" (
     session_id bigserial not null primary key,
     session_uid varchar(42) not null,
     last_access timestamp not null default(now()),
     is_persistant boolean default(false) not null,
     device_id varchar(255) null,
+    addresss varchar(16) null,
     user_id bigint null,
     constraint fk_session_user_is
         foreign key ( user_id ) references web."user" ( user_id )
